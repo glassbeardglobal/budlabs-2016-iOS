9d4285673d7395246e8ee7b6dcb6c521648963f1
Rahul Surti <rahulsurti97@gmail.com>
2016-09-17 17:31:34 -0500

    Converted to Swift 3 in Xcode 8

--- BarleyNet/BarleyNet/ViewController.swift
+++ BarleyNet/BarleyNet/ViewController.swift
@@ -14,91 +14,69 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        getRequest(withURL: barley);
-        postRequest(withURL: barley, andPost:"test=You Bitch");
+        
+        getRequest(withURL: barley)
+        postRequest(withURL: barley, andPost: "test=You Bitch")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    func getJSON(withString s:String) {
-        let url = NSURL(string: s)!
-        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) -> Void in
-            if let urlContent = data {
-                do {
-                    let jsonResult = try NSJSONSerialization.JSONObjectWithData(urlContent, options: NSJSONReadingOptions.MutableContainers)
-                    if let message = jsonResult["message"] as? String {
-                        print(message)
-                    } else {
-                        print("nil value")
-                    }
-                } catch {
-                    print("JSON serialization failed")
-                }
-            }
-        }
-        task.resume()
-    }
     
     func getRequest(withURL url:String) {
-        let request = NSMutableURLRequest(URL: NSURL(string: url)!)
-        request.HTTPMethod = "GET"
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
-            guard error == nil && data != nil else {
-                // check for fundamental networking error
-                print("error=\(error)")
+        let myURL = URL(string: url)
+        var request = URLRequest(url: myURL!)
+        request.httpMethod = "GET"
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                print(error)
+                return
+            }
+            guard let data = data else {
+                print("Data is empty")
                 return
             }
             
-            if let httpStatus = response as? NSHTTPURLResponse where httpStatus.statusCode != 200 {
+            if let httpStatus = response as? HTTPURLResponse , httpStatus.statusCode != 200 {
                 // check for http errors
                 print("statusCode should be 200, but is \(httpStatus.statusCode)")
                 print("response = \(response)")
-                
             }
-            
-            if let urlContent = data {
-                do {
-                    print("getRequest:")
-                    let jsonResult = try NSJSONSerialization.JSONObjectWithData(urlContent, options: NSJSONReadingOptions.MutableContainers)
-                    print(jsonResult)
-                } catch {
-                    print("JSON serialization failed")
-                }
+            do {
+                print("getRequest:")
+                let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers)
+                print(json)
+            } catch {
+                print("JSON serialization failed")
             }
         }
         task.resume()
     }
     
     func postRequest(withURL url:String, andPost post:String) {
-        let request = NSMutableURLRequest(URL: NSURL(string: url)!)
-        request.HTTPMethod = "POST"
+        let myURL = URL(string: url)
+        var request = URLRequest(url: myURL!)
+        request.httpMethod = "POST"
         let postString = post
-        request.HTTPBody = postString.dataUsingEncoding(NSUTF8StringEncoding)
-        let task = NSURLSession.sharedSession().dataTaskWithRequest(request) { data, response, error in
-            guard error == nil && data != nil else {
-                // check for fundamental networking error
+        request.httpBody = postString.data(using: .utf8)
+        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+            guard let data = data, error == nil else {                                                 // check for fundamental networking error
                 print("error=\(error)")
                 return
             }
             
-            if let httpStatus = response as? NSHTTPURLResponse where httpStatus.statusCode != 200 {
-                // check for http errors
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {           // check for http errors
                 print("statusCode should be 200, but is \(httpStatus.statusCode)")
                 print("response = \(response)")
-                
             }
             
-            if let urlContent = data {
-                do {
-                    print("postRequest:")
-                    let jsonResult = try NSJSONSerialization.JSONObjectWithData(urlContent, options: NSJSONReadingOptions.MutableContainers)
-                    print(jsonResult)
-                } catch {
-                    print("JSON serialization failed")
-                }
+            do {
+                print("postRequest:")
+                let json = try JSONSerialization.jsonObject(with: data, options: JSONSerialization.ReadingOptions.mutableContainers)
+                print(json)
+            } catch {
+                print("JSON serialization failed")
             }
         }
         task.resume()
